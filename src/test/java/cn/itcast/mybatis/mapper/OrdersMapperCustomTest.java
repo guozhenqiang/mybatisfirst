f803658ad164dfa7b20cb688aa7047fe2f2b3a6e
guozhenqiang <1003704757@qq.com>
2017-09-20 00:33:05 +0800

    add LazyLoading

--- src/test/java/cn/itcast/mybatis/mapper/OrdersMapperCustomTest.java
+++ src/test/java/cn/itcast/mybatis/mapper/OrdersMapperCustomTest.java
@@ -63,5 +63,57 @@ public class OrdersMapperCustomTest {
 		
 		System.out.println(list);
 	}
+	
+	@Test
+	public void testFindOrdersAndOrderDetailResultMap() throws Exception {
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		//创建UserMapper对象，mybatis自动生成mapper代理对象
+		OrdersMapperCustom ordersMapperCustom = sqlSession.getMapper(OrdersMapperCustom.class);
+		
+		//调用userMapper的方法
+		
+		List<Orders> list = ordersMapperCustom.findOrdersAndOrderDetailResultMap();
+		
+		sqlSession.close();
+		
+		System.out.println(list);
+	}
+	
+	@Test
+	public void testFindUserAndItemsResultMap() throws Exception {
+		
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		//创建UserMapper对象，mybatis自动生成mapper代理对象
+		OrdersMapperCustom ordersMapperCustom = sqlSession.getMapper(OrdersMapperCustom.class);
+		
+		//调用userMapper的方法
+		
+		List<User> list = ordersMapperCustom.findUserAndItemsResultMap();
+		
+		sqlSession.close();
+		
+		System.out.println(list);
+	}
+	
+	// 查询订单关联查询用户，用户信息使用延迟加载
+	@Test
+	public void testFindOrdersUserLazyLoading() throws Exception {
+		SqlSession sqlSession = sqlSessionFactory.openSession();// 创建代理对象
+		OrdersMapperCustom ordersMapperCustom = sqlSession.getMapper(OrdersMapperCustom.class);
+		// 查询订单信息（单表）
+		List<Orders> list = ordersMapperCustom.findOrdersUserLazyLoading();
+
+		// 遍历上边的订单列表
+		for (Orders orders : list) {
+			// 执行getUser()去查询用户信息，这里实现按需加载
+			User user = orders.getUser();
+			System.out.println(user);
+		}
+
+	}
+	
 
 }
